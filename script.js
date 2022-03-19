d026f82b225a1a065f3eb8689acce0508910c986
Taltos87 <93266473+Taltos87@users.noreply.github.com>
2022-03-19 21:49:58 +0000

    All data works
    added few JS features

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function  displayTempreature(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector(`#temperture`);
+  let cityElement = document.querySelector(`#city`);
+  let descriptionElement = document.querySelector(`#description`);
+
+
+ temperatureElement.innerHTML = Math.round ( response.data.main.temp);
+ cityElement.innerHTML = response.data.name;
+ descriptionElement.innerHTML= response.data.weather[0].description;
+
+ console.log(response.data);
 }
-let apiKey =  "2bcda0ef514ca396d716955408357744"
+let apiKey =  "2bcda0ef514ca396d716955408357744";
 let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTempreature);
