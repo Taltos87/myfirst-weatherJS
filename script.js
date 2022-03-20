52a9d1a13ea890dda0c1954719a136701b50b6cb
Taltos87 <93266473+Taltos87@users.noreply.github.com>
2022-03-20 20:32:54 +0000

    fix the typo

--- script.js
+++ script.js
@@ -2,14 +2,19 @@ function  displayTempreature(response) {
   let temperatureElement = document.querySelector(`#temperture`);
   let cityElement = document.querySelector(`#city`);
   let descriptionElement = document.querySelector(`#description`);
-
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
 
  temperatureElement.innerHTML = Math.round ( response.data.main.temp);
  cityElement.innerHTML = response.data.name;
  descriptionElement.innerHTML= response.data.weather[0].description;
+ humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+
 
  console.log(response.data);
 }
+function seach(city){}
 let apiKey =  "2bcda0ef514ca396d716955408357744";
 let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
