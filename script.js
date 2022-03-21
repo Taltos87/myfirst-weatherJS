5c6fec92891b55f81a5bf58a8926ee4e31ac42ed
Taltos87 <93266473+Taltos87@users.noreply.github.com>
2022-03-21 19:54:53 +0000

    Try again tomorrow
    Test over now is not working again the city after added icons

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ function formatDate(timestamp) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector(`#date`);
+  let iconElement = document.querySelector(`#icon`);
 
  temperatureElement.innerHTML = Math.round ( response.data.main.temp);
  cityElement.innerHTML = response.data.name;
@@ -36,12 +37,16 @@ function formatDate(timestamp) {
  humidityElement.innerHTML = response.data.main.humidity;
 windElement.innerHTML = Math.round(response.data.wind.speed);
 dateElement.innerHTML = formatDate(response.data.dt * 1000);
+iconElement.setAttribute(
+  "src", 
+  `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` );
+iconElement.setAttribute("alt" , response.data.weather[0].description);
 
  console.log(response.data);
 }
 function seach(city){}
 let apiKey =  "2bcda0ef514ca396d716955408357744";
-let city = "New York";
+let city = "Oxford";
 let apiUrl =  `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 console.log(apiUrl);
 axios.get(apiUrl).then(displayTempreature);
