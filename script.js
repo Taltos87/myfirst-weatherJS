220dba0d2c8f591b7004142447b7038b1ab0950a
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-05-24 22:59:41 +0100

    Punctuation  });

--- script.js
+++ script.js
@@ -35,26 +35,12 @@ let celsiusTemp = null;
 
 function displayForecast() {
     let forecastElement = document.querySelector("#forecast");
-    
-    let days = ["Thu", "Fri", "Sat", "Sun"];
-
-
     let forecastHTML= `<div class="row">`;
-    forecastHTML = forecastHTML + `
-    <div class="col-2">
-     <div class="weather-forecast-date">Wed</div> 
-     <img
-     src="http://openweathermap.org/img/wn/01d.png"
-     alt=""
-     width="42" />
-     <div class="weather-forecast-temperatures">
-         <span class="weather-forecast-temperature-max"> 18° </span>
-         <span class="weather-forecast-temperature-min"> 12° </span>  
-    </div>
-</div>`; 
+    let days = ["Thu", "Fri", "Sat", "Sun"];
+    days.forEach(function (day) {
 forecastHTML = forecastHTML + `
 <div class="col-2">
- <div class="weather-forecast-date">Wed</div> 
+ <div class="weather-forecast-date">${day}</div> 
  <img
  src="http://openweathermap.org/img/wn/01d.png"
  alt=""
@@ -64,7 +50,8 @@ forecastHTML = forecastHTML + `
      <span class="weather-forecast-temperature-min"> 12° </span>  
 </div>
 </div>`; 
-    forecastHTML = forecastHTML + `</div>`;
+    });
+ forecastHTML = forecastHTML + `</div>`;
  forecastElement.innerHTML = forecastHTML;
 }
 
