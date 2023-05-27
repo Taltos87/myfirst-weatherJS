04c8b2f80e6483db8692cc5e18ef671d048871f2
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-05-27 17:19:38 +0100

    formatDay(forcastDay.dt)

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function formatDate(timestamp) {
     ];
     let day = days[date.getDay()];
 
-    return `${day} ${hours}: ${minutes}`;
+    return `${day}${hours}:${minutes}`;
 }
 
 let fahrenheitLink = document.querySelector("#fahrenheit-link");
@@ -30,37 +30,46 @@ celsiusLink.addEventListener("click", showCelsiusTemp);
 
 let celsiusTemp = null;
 
+ function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+    return days[day];
+ }
 
 
 
-function displayForecast() {
-    function displayForecast(response) {
-        console.log(response.data.daily);
-    }
-    
-    // function formatDay(timestamp) {
-    //     let date = new Date(timestamp * 1000);
-    //     let day = date.getDay();
-    //     let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue" ];
-    //     return days[day];
-    // }
+function displayForecast(response) {
+    let forecast = response.data.daily;
     let forecastElement = document.querySelector("#forecast");
     let forecastHTML= `<div class="row">`;
-    let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue" ];
-    days.forEach(function (day) {
-forecastHTML = forecastHTML + `
-<div class="col-2">
- <div class="weather-forecast-date">${day}</div> 
- <img
- src="http://openweathermap.org/img/wn/01d.png"
- alt=""
- width="42" />
- <div class="weather-forecast-temperatures">
-     <span class="weather-forecast-temperature-max"> 18° </span>
-     <span class="weather-forecast-temperature-min"> 12° </span>  
-</div>
-</div>`; 
-    });
+
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6) {
+            forecastHTML += `
+              <div class="col-2">
+                <div class="weather-forecast-date">${formatDay(
+                  forecastDay.dt
+                )}</div> 
+                <img
+                  src="http://openweathermap.org/img/wn/${
+                    forecastDay.weather[0].icon
+                  }@2x.png"
+                  alt=""
+                  width="42"
+                />
+                <div class="weather-forecast-temperatures">
+                  <span class="weather-forecast-temperature-max">${Math.round(
+                    forecastDay.temp.max
+                  )}°</span>
+                  <span class="weather-forecast-temperature-min">${Math.round(
+                    forecastDay.temp.min
+                  )}°</span>
+                </div>
+              </div>`;
+          }
+        });
  forecastHTML = forecastHTML + `</div>`;
  forecastElement.innerHTML = forecastHTML;
 }
