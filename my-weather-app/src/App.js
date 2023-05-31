84d573ecf5b20db12d4095dbc8220429978de8a4
Akelarre Taltos <93266473+Taltos87@users.noreply.github.com>
2023-05-31 22:27:23 +0100

    Revert "a lot of typo issues, crashed app"
    This reverts commit 394deab7820f548b23207feda9c557b34f422cdc.

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -1,50 +1,23 @@
-import React, { useState } from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Form from './components/Form';
-import WeatherCard from './components/WeatherCard';
-
-const API_KEY = '2bcda0ef514ca396d716955408357744';
 
 function App() {
-  const [city, setCity] = useState('');
-  const [weatherData, setWeatherData] = useState(null);
-  const [error, setError] = useState('');
-
-  const handleInputChange = (event) => {
-    setCity(event.target.value);
-  };
-
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    try {
-      const response = await fetch(
-        `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
-      );
-
-      if (!response.ok) {
-        throw new Error('City not found');
-      }
-
-      const data = await response.json();
-      setWeatherData(data);
-      setError('');
-    } catch (error) {
-      setWeatherData(null);
-      setError('City not found');
-    }
-  };
-
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <Form
-        city={city}
-        handleInputChange={handleInputChange}
-        handleSubmit={handleSubmit}
-      />
-      {error && <p className="error">{error}</p>}
-      {weatherData && <WeatherCard weatherData={weatherData} />}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
